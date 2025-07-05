e6621e0e63800ff4a2b8e75273dbe1415ff69f3f
Sameer <samnehal2015@gmail.com>
2025-07-05 21:11:21 +0530

    Initial commit: project setup with client and server folders

---
+++ server/server.js
@@ -0,0 +1,33 @@
+
+const express = require('express'); // Express to create server
+
+const mongoose = require('mongoose'); // Mongoose to connect to MongoDB
+
+const cors = require('cors'); // CORS to allow cross-origin requests
+
+require('dotenv').config(); // dotenv to load variables from .env file 
+
+// Create an express app
+const app = express();
+
+// Define a PORT
+const PORT = process.env.PORT || 5000;
+
+// Using Middlewares
+app.use(cors());  // allow frontend access
+app.use(express.json()); // to parse JSON requests
+
+// Using Routes
+app.use('api/books', require('./routes/bookRoutes'));
+
+
+// Connect to MongoDb and starting the server
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("MongoDB connected")
+        app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
+    })
+    .catch(err => console.log(err)); 

1b17814c0f23d19607830336edac4f4ed57993b9
Sameer <samnehal2015@gmail.com>
2025-07-08 01:41:47 +0530

    Added book routes and fixed import issues

---
+++ server/models/Book.js
@@ -0,0 +1,33 @@
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    author: {
+        type: String,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ['Reading', 'Wishlist', 'Completed'],
+        default: "Reading"
+    },
+
+    userId: {
+        type: String,
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+
+})
+
+module.exports = mongoose.model('Book', bookSchema); 
